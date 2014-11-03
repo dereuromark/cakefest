9e798c3d5230f2742c0fd0c7586818933d4630df
euromark <euromark@web.de>
2014-11-03 01:26:40 +0100

    Correct helper.

--- Shell/AppShell.php
+++ Shell/AppShell.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * AppShell file
- *
- * PHP 5
- *
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         CakePHP(tm) v 2.0
- */
 
 App::uses('Shell', 'Console');
 
