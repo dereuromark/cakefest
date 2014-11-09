4e4ac572aa2dfe4d12605327a912c1fa42e079f1
euromark <euromark@web.de>
2014-11-10 00:39:09 +0100

    tests/bootstrap

--- Config/paths.php
+++ Config/paths.php
@@ -11,7 +11,6 @@
  * @since         3.0.0
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-namespace config;
 
 /**
  * Use the DS to separate the directories in other defines
