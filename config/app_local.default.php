ee3d5e869f0ab51783f832ee8b4c14203304bdbf
Mark Scherer <euromark@web.de>
2015-01-23 02:09:24 +0100

    default app_local.default.php

---
+++ config/app_local.default.php
@@ -0,0 +1,39 @@
+<?php
+$config = [
+	'debug' => true,
+
+	'Security' => [
+		'salt' => '1ebcb009bb3f8ebe43a4addc3fc1c1f310c50520',
+	],
+
+	'Datasources' => [
+		'default' => [
+			'host' => 'localhost',
+			'username' => 'root',
+			'password' => '',
+			'database' => 'cake_cakefest',
+		],
+
+		/**
+		 * The test connection is used during the test suite.
+		 */
+		'test' => [
+			'host' => 'localhost',
+			'username' => 'root',
+			'password' => '',
+			'database' => 'cake_test',
+		],
+	],
+
+	'Config' => array(
+		'adminEmail' => 'youreemail@example.de'
+	),
+
+	'FormConfig' => array(
+		'novalidate' => true,
+		'templates' => array(
+			'dateWidget' => '{{day}}{{month}}{{year}}{{hour}}{{minute}}{{second}}{{meridian}}',
+		)
+	)
+
+];
