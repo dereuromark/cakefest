d5629d6f94d1c4038b616e17c0d5a8a63195ad79
Mark Scherer <euromark@web.de>
2015-01-25 23:34:46 +0100

    hasher

--- config/app_custom.php
+++ config/app_custom.php
@@ -13,20 +13,28 @@ return [
 		'timeout' => 3 * DAY,
 		'cookieTimeout' => MONTH
 	],
+
+	'Passwordable'  => [
+		'passwordHasher' => ['className' => 'Fallback', 'hashers' => ['Default', 'Weak']]
+	],
+
 	'Roles' => array(
 		'admin' => '1',
 		'user' => '2',
 	),
+
 	'Config' => array(
 		'pageName' => 'CakeFest App',
 		'adminEmail' => '', // Overwrite in app_local.php
 		'adminName' => 'Mark',
 		'rememberMe' => false,
 	),
+
 	'Asset' => array(
 		'js' => 'buffer'
 	),
-/* Please provide the following using the configs_private.php which is not under version control */
+
+  // Please provide the following using the app_locale.php which is not under version control
 	'Email' => array(
 		'Smtp' => array(
 			'host' => '',
