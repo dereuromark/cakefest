6757e1f2c6ab853304f90e0012ad656f88659270
Mark Scherer <euromark@web.de>
2015-02-03 22:07:27 +0100

    script

--- build.sh
+++ build.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-echo "### INSTALL ###";
+# Warning: This is NOT a productive script, but for local dev envs only!
 
+echo "### INSTALL/UPDATE ###";
 php composer.phar selfupdate
 
 git pull
 
-php composer.phar update --prefer-dist --no-dev --optimize-autoloader
+php composer.phar update --prefer-dist --no-dev --optimize-autoloader --no-interaction
+
+#echo "### DB MIGRATION ###";
+# Don't forget to "chmod +x bin/cake" first
+#bin/cake Migrations migrate
 
 echo "### CLEANUP ###";
-
 rm -rf ./tmp/cache/models/*
 rm -rf ./tmp/cache/persistent/*
 
 chown -R www-data:www-data *
 
 echo "### DONE ###";
-exit
+exit;
