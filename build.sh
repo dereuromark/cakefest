9fd5dc284dffd5f97c730f987362f5b5c525169d
Mark Scherer <euromark@web.de>
2015-10-12 01:48:05 +0200

    build script

--- build.sh
+++ build.sh
@@ -2,21 +2,34 @@
 # Warning: This is NOT a productive script, but for local dev envs only!
 
 echo "### INSTALL/UPDATE ###";
+[ ! -f composer.phar ] && curl -sS https://getcomposer.org/installer | php
 php composer.phar selfupdate
 
 git pull
 
-php composer.phar update --prefer-dist --no-dev --optimize-autoloader --no-interaction
+php composer.phar install --prefer-dist --no-dev --optimize-autoloader --no-interaction
+
+chmod +x bin/cake
+
+mkdir -p ./tmp
+mkdir -p ./logs
+mkdir -p ./webroot/js/cjs/
+mkdir -p ./webroot/css/ccss/
 
 #echo "### DB MIGRATION ###";
 # Don't forget to "chmod +x bin/cake" first
 #bin/cake Migrations migrate
+
+echo "### ASSETS ###";
+bin/cake AssetCompress.AssetCompress build
 
 echo "### CLEANUP ###";
 rm -rf ./tmp/cache/models/*
 rm -rf ./tmp/cache/persistent/*
 
 chown -R www-data:www-data *
+chmod -R 0770 ./tmp
+chmod -R 0770 ./webroot/js/cjs/
+chmod -R 0770 ./webroot/css/ccss/
 
 echo "### DONE ###";
-exit;
